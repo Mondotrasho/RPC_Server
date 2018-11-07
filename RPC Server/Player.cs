2fad33d7084c9694ca8e557fb499ea4aac899b17
Mondotrasho <oscar@peoplepictures.com.au>
2018-11-07 20:11:20 +1100

    Reciever setup

---
+++ RPC Server/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Player : MarshalByRefObject
+{
+    private int HorizontalOffset; // half of 28
+    private int VerticalOffset;// half of 10
+    public void SayHello(string[] text)
+    {
+        HorizontalOffset = text[0].Length / 2;
+        VerticalOffset = text.Length / 2;
+
+        for (int i = 0; i < 10; i++)
+        {
+            //Console.WriteLine(String.Format("{0," + (Console.WindowWidth / 2) + "}",text));
+            int leftOffSet = (Console.WindowWidth / 2 - HorizontalOffset);
+            int topOffSet = (Console.WindowHeight / 2 - VerticalOffset + i);
+            Console.SetCursorPosition(leftOffSet, topOffSet);
+            Console.Write(text[i]);
+        }
+    }
+}
--- RPC Server/Program.cs
+++ RPC Server/Program.cs
@@ -1,17 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
 
-namespace RPC_Server
-{
     class Program
     {
         static void Main(string[] args)
         {
+            //Create Server
+            TcpServerChannel channel = new TcpServerChannel(1337);
+            ChannelServices.RegisterChannel(channel, false);
 
+            //Register Player
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(Player),"Little",WellKnownObjectMode.SingleCall);
 
+            //Pause
+            Console.WriteLine("Listening for requests.\nPress enter to exit...\n");
+            Console.ReadLine();
         }
     }
-}
